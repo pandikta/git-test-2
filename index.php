5cce34eeaa16e21d409c779857818a3d7c7370cf
Pandikta <ardhianwira@gmail.com>
2020-10-07 16:11:22 +0630

    nambah paragraf lorem

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi tempore cum et mollitia aut. Ad minus molestias sequi voluptas fuga cum? Deleniti, repellendus similique. Incidunt sed veritatis vitae ea neque.</p>
 </body>
 
 </html>
